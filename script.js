d50d59a1c07798a96100d6125afb5bf6599806ab
Frank Buono <fbuonojr@fordham.edu>
2020-10-22 12:50:59 -0400

    added reset function and changed some class to id

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-    
+    //global variables
+    var firstNumber = 0;
+    var secondNumber = 0;
+    var operator = "";
+    var result = 0;
+    var isOperatorChosen = false;
+    var isCalculated = false;
+
+    //function to reset our calculator
+    function initializeCalculator() {
+        firstNumber = 0;
+        secondNumber = 0;
+        operator = "";
+        result = 0;
+        isOperatorChosen = false;
+        isCalculated = false;
+
+        $("#first-number, #second-Number, #operator, #result").empty();
+    }
 })
