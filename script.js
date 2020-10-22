209ba4e7d63d34ba16734ca1c5f5c1dbd627cd62
Frank Buono <fbuonojr@fordham.edu>
2020-10-22 12:56:30 -0400

    added click function for number buttons

--- script.js
+++ script.js
@@ -18,4 +18,22 @@ $(document).ready(function() {
 
         $("#first-number, #second-Number, #operator, #result").empty();
     }
+
+    //click function for number buttons
+    $(".number").on("click", function(){
+        //see if calculation has already been done
+        if(isCalculated){
+            return false;
+        }
+
+        // check to see if operator has been chosen: if yes write second number if no write first
+        if(isOperatorChosen){
+            secondNumber += $(this).vaL();
+            $("#second-number").text(secondNumber);
+        }
+        else {
+            firstNumber += $(this).vaL();
+            $("#first-number").text(firstNumber);
+        }
+    });
 })
